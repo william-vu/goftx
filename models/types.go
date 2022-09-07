3bf4941ae4cc5c671f116b0b98e546dcc1170a45
william <william.vu@pluang.com>
2022-09-07 22:53:33 +0800

    wip

--- models/types.go
+++ models/types.go
@@ -107,8 +107,8 @@ func (f *FTXTime) UnmarshalJSON(data []byte) error {
 	sec, nsec := math.Modf(t)
 	// f.Time = time.Unix(int64(sec), int64(nsec))
 	// f.Time = time.Unix(int64(sec), int64(nsec))
-	// fmt.Println("109", sec)
-	// fmt.Println("110", nsec)
+	fmt.Println("109", sec)
+	fmt.Println("110", nsec)
 	f.Time = time.UnixMilli(int64(sec)*1000 + int64(nsec*1000))
 	return nil
 }
